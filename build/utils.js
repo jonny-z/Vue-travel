7f6b34639f0a0c4f40a8b976268d06badab2a23a
john <john@192.168.0.104>
2018-05-19 12:14:43 +0800

    scss init

--- build/utils.js
+++ build/utils.js
@@ -53,14 +53,38 @@ exports.cssLoaders = function (options) {
       return ['vue-style-loader'].concat(loaders)
     }
   }
-
+  function resolveResouce(name) {
+      return path.resolve(__dirname, '../src/assets/scss/' + name);
+  }
+  function generateSassResourceLoader() {
+      var loaders = [
+           cssLoader,
+           // 'postcss-loader',
+           'sass-loader',
+           {
+               loader: 'sass-resources-loader',
+               options: {
+                 // it need a absolute path
+                 resources: [resolveResouce('style.scss')]
+               }
+           }
+      ];
+      if (options.extract) {
+           return ExtractTextPlugin.extract({
+             use: loaders,
+             fallback: 'vue-style-loader'
+           })
+      } else {
+           return ['vue-style-loader'].concat(loaders)
+      }
+  }
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    sass: generateSassResourceLoader('sass', { indentedSyntax: true }),
+    scss: generateSassResourceLoader('sass'),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
