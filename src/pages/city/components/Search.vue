035969fd488a98e64dc21f977a576807a20fb174
john <john@192.168.0.104>
2018-05-27 11:35:52 +0800

    city header

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="search">
+		<input class="search-input" type="text" placeholder="输入城市名或拼音">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CitySearch'
+}
+</script>
+
+<style lang="scss" scoped>
+	.search {
+		height: .72rem;
+		background-color: $site-bg-color;
+		padding: 0 .1rem;
+		.search-input {
+			width: 100%;
+			height: .62rem;
+			line-height: .62rem;
+			border-radius: .06rem;
+			text-align: center;
+			padding: 0 .1rem;
+			color: #666;
+			box-sizing: border-box;
+		}
+	}
+</style>
--- src/pages/home/component/Header.vue
+++ src/pages/home/component/Header.vue
@@ -7,10 +7,12 @@
 			<i class="iconfont">&#xe632;</i>
 			输入城市/景点/游玩
 		</div>
-		<div class="header-right">
-			{{this.city}}
-			<i class="iconfont arrow-icon">&#xe64a;</i>
-		</div>
+		<router-link to='/city'>
+			<div class="header-right">
+				{{this.city}}
+				<i class="iconfont arrow-icon">&#xe64a;</i>
+			</div>
+		</router-link>
 	</div>
 </template>
 
@@ -52,6 +54,7 @@ export default {
 			width: 1.24rem;
 			float: right;
 			text-align: center;
+			color: #fff;
 			.arrow-icon {
 				font-size: .24rem;
 				margin-left: -.04rem;
